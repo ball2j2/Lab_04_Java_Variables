e18036811296ae7ea15c6079e4bc003393d39344
jordanball <ball2j2@mail.uc.edu>
2025-09-19 18:58:45 -0400

    Init

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,58 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Init commit");
-        int intOperandA = 1;
-        int intOperandB = 2;
-        int intSum = 5;
-        int intProduct = 10;
-        int intDifference = 24;
-        int intQuotient = 22;
-        int intModulo = 7;
-        System.out.println("total value is " + intOperandA + intOperandB + intSum + intProduct);
+        int intOperandA = 10;
+        int intOperandB = 3;
+        int intSum;
+        int intProduct;
+        int intDifference;
+        int intQuotient;
+        int intModulo;//TIP Pre
 
-        double doubleOperandA = 3.50;
-        double doubleOperandB = 4.50;
-        double doubleSum = 5.50;
-        double doubleProduct = 6.50;
-        double doubleDifference = 7.50;
-        double doubleQuotient = 8.50;
-      }
+        double doubleOperandA = 5.5;
+        double doubleOperandB = 2.0;
+        double doubleSum;
+        double doubleProduct;
+        double doubleDifference;
+        double doubleQuotient;
+
+        int numberOfSiblings = 2;
+        boolean isRaining = false;
+        double gasPricePerGallon = 4.49;
+        int favoriteNumber = 1;
+        double shoeSize = 12;
+        String birthMonth = "July";
+        String fullName = "Jordan Ball";
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum using ints of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The product using ints of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+
+        intDifference = intOperandA - intOperandB;
+        System.out.println("The difference using ints of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+
+        intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient using ints of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+
+        intModulo = intOperandA % intOperandB;
+        System.out.println("The modulo using ints of " + intOperandA + " and " + intOperandB + " is " + intModulo);
+
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
+        doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The product using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+
+        doubleDifference = doubleOperandA - doubleOperandB;
+        System.out.println("The difference using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+
+        doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The quotient using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+
+// ss <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+    }
 }
