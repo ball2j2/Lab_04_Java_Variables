fa5c4cf4acf0bd7135981a1de6603c1a7de324be
jordanball <ball2j2@mail.uc.edu>
2025-09-19 18:32:07 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Init commit");
+        int intOperandA = 1;
+        int intOperandB = 2;
+        int intSum = 5;
+        int intProduct = 10;
+        int intDifference = 24;
+        int intQuotient = 22;
+        int intModulo = 7;
+        System.out.println("total value is " + intOperandA + intOperandB + intSum + intProduct);
+
+        double doubleOperandA = 3.50;
+        double doubleOperandB = 4.50;
+        double doubleSum = 5.50;
+        double doubleProduct = 6.50;
+        double doubleDifference = 7.50;
+        double doubleQuotient = 8.50;
+      }
+}
